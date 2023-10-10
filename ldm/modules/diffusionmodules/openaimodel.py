934aeeeed10d3a34a8db966a6e888114ed162e79
taehyeonEum <paul994782@gmail.com>
2023-10-10 16:42:57 +0900

    change openaimodel.py

--- ldm/modules/diffusionmodules/openaimodel.py
+++ ldm/modules/diffusionmodules/openaimodel.py
@@ -438,8 +438,6 @@ class UNetModel(nn.Module):
 
         # input tensor  
         h = th.tensor(input["x"]).to('cuda')
-        print("input['inpainting_extra_input'] type \n", type(input["inpainting_extra_input"]))
-        input["inpainting_extra_input"] = th.tensor(input["inpainting_extra_input"]).to('cuda')
         if self.downsample_net != None and self.first_conv_type=="GLIGEN":
             temp  = self.downsample_net(input["grounding_extra_input"])
             h = th.cat( [h,temp], dim=1 )
@@ -447,6 +445,8 @@ class UNetModel(nn.Module):
             if self.downsample_net != None:
                 breakpoint() # TODO: think about this case 
             print("thum edit\n", type(th.cat( [h, input["inpainting_extra_input"]], dim=1 )))
+            print("input['inpainting_extra_input'] type \n", type(input["inpainting_extra_input"]))
+            input["inpainting_extra_input"] = th.tensor(input["inpainting_extra_input"]).to('cuda')
             h = th.tensor(th.cat( [h, input["inpainting_extra_input"]], dim=1 )).to('cuda')
         
         # Text input 
